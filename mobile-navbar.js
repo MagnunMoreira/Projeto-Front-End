6415c191454ffd3c264ad9eeeb20a8c2f05239b8
MAGNUN ALVES MOREIRA <magnummoreira02@hotmail.com>
2022-08-21 09:05:16 -0400

    finalizado responsividade do menu para mobile

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,32 +1,47 @@
 class mobilenavbar {
+
     constructor(mobilemenu, navlist, navlinks) {
-    this.mobilemenu = document.querySelector(mobilemenu);
-    this.navlist = document.querySelector(navlist);
-    this.navlinks = document.querySelectorAll(navlinks);
-    this.activeclass = "active";     
+        this.mobilemenu = document.querySelector(mobilemenu);
+        this.navlist = document.querySelector(navlist);
+        this.navlinks = document.querySelectorAll(navlinks);
+        this.activeclass = "active";
+
+        
+        this.handleclick = this.handleclick.bind(this);
     }
 
-   handleclick(){
-    console.log(this);
-    this.navlist.classlist.toggle(this.activeclass);
-   }
+    animateLinks(){
+        this.navlinks.forEach((link, index) => {
+           link.style.animation
+           ?(link.style.animation = "") 
+           :(link.style.animation = `navlinkfade 0.5s ease forwards ${index / 7 + 0.3}s`);
 
-   addclickevent() {
-    this.mobilemenu.addeventlistener("click", this.handleclick);
-   }
+        });
+    }
 
-    init(){
-        if (this.mobilemenu){
+     handleclick() {
+        this.navlist.classList.toggle(this.activeclass);
+        this.mobilemenu.classList.toggle(this.activeclass);
+        this.animateLinks();
+    }
+
+    addclickevent() {
+        this.mobilemenu.addEventListener("click", this.handleclick);
+    }
+
+
+    init() {
+        if (this.mobilemenu) {
             this.addclickevent();
-
         }
         return this;
     }
 }
 
-const mobilenavbar = new mobilenavbar(
+const mobilenavbars = new mobilenavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
 );
-mobilenavbar.init();
+
+mobilenavbars.init();
